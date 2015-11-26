06495ce405bb6adc79fddef5e8cd36f201a0f287
Alex Andriishyn <alex.andriishyn@icloud.com>
2015-11-26 15:25:12 -0500

    Change ticket.js model

---
+++ models/ticket.js
@@ -0,0 +1,45 @@
+/**
+* Anthony Scinocco
+* incident-management.azurewebsites.net
+* November 23, 2015
+* Holds the Ticket schema
+*/
+//modules for schema
+var mongoose = require('mongoose');
+
+//build schema object
+var Schema = mongoose.Schema;
+
+var IncidentSchema = new Schema({
+  userId: String,
+  description: String,
+  //storing priority as a number
+  //for easy comparison
+  priority: Number,
+  //storing status as a number for
+  //easy comparison
+  status: Number,
+  //narrative stored as nested document for
+  //modularity
+  narrative: [
+    {
+      narrativeId: String,
+      narrativeTitle: String,
+      narrativeBody: String,
+      narrativeCreatedAt: {
+        type: Date,
+        default: Date.now
+      },
+      narrativeUpdatedAt: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ]
+},
+{
+  //specify the collection
+  collection: 'incidents'
+});
+
+module.exports = mongoose.model('Incident', IncidentSchema);
