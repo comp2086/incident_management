2b2edf2c0c5acf36e4bc61d644d47c937300ff1b
FlyingArs <davidyu89tech@gmail.com>
2015-12-13 10:07:49 -0500

    Update the edit and view pages
    Trying to change the routes on users.client.routes to render certain
    page, but to no avail

--- public/users/config/users.client.routes.js
+++ public/users/config/users.client.routes.js
@@ -1,12 +1,25 @@
 // Invoke 'strict' JavaScript mode
 'use strict';
 
-// Configure the 'articles' module routes
+// Configure the 'users' module routes
 angular.module('users').config(['$routeProvider',
-	function($routeProvider) {
-		$routeProvider.
-		when('/', {
-			templateUrl: 'users/views/view-users.client.view.html'
-		});
-	}
+    function ($routeProvider) {
+        $routeProvider
+            .when('/',
+                {
+                    templateUrl: 'users/views/view-users.client.view.html'
+                })
+            .when('/:userId',
+                {
+                    templateUrl: 'users/views/view-users.client.view.html'
+                })
+            .when('/:userId/edit',
+                {
+                    templateUrl: 'users/views/edit-article.client.view.html'
+                })
+            .otherwise(
+                {
+                    redirectTo: 'users/views/view-users.client.view.html'
+                });
+    }
 ]);
