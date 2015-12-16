d535f194cbe936808369a129d2a0875e9bb73027
Alex Andriishyn <alex.andriishyn@icloud.com>
2015-12-16 14:45:04 -0500

    Fixing azure

--- server.js
+++ server.js
@@ -6,22 +6,25 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 // Load the module dependencies
 var mongoose = require('./config/mongoose'),
 	express = require('./config/express'),
-	passport = require('./config/passport');
+	passport = require('./config/passport'),
+	http = require('http');
+
+var app = express();
 
 // Create a new Mongoose connection instance
 var db = mongoose();
-
-// Create a new Express application instance
-var app = express();
 
 // Configure the Passport middleware
 var passport = passport();
 
 // Use the Express application instance to listen to the '3000' port
-app.listen(3001);
+app.set('port', 3000);
 
-// Log the server status to the console
-console.log('Server running at http://localhost:3000/');
+var server = http.createServer(app);
+
+server.listen(3000);
+//server.on('error', onError);
+//server.on('listening', onListening);
 
 // Use the module.exports property to expose our Express application instance for external usage
 module.exports = app;
