b8ee7b10fd674bebcade9f2009b328b1cbb55d73
Alex Andriishyn <alex.andriishyn@icloud.com>
2015-12-16 15:03:45 -0500

    Fixing azure-2

--- server.js
+++ server.js
@@ -8,8 +8,11 @@ var mongoose = require('./config/mongoose'),
 	express = require('./config/express'),
 	passport = require('./config/passport'),
 	http = require('http');
+var debug = require('debug')('incident_management:server');
 
 var app = express();
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
 
 // Create a new Mongoose connection instance
 var db = mongoose();
@@ -17,14 +20,67 @@ var db = mongoose();
 // Configure the Passport middleware
 var passport = passport();
 
-// Use the Express application instance to listen to the '3000' port
-app.set('port', 3000);
-
 var server = http.createServer(app);
 
 server.listen(3000);
-//server.on('error', onError);
-//server.on('listening', onListening);
+server.on('error', onError);
+server.on('listening', onListening);
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
 
 // Use the module.exports property to expose our Express application instance for external usage
 module.exports = app;
